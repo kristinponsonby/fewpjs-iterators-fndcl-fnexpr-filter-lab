2227a07d0357be6fc01561c26f081aa354754835
Kristin <kristinponsonby@gmail.com>
2021-08-19 13:16:22 -0400

    commit tree bcc54331e13f0f2396ab3713b23afd8245fb6ecc

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 //  const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
-
-findMatching = (drivers) => {
-     let newDrivers = drivers.filter(n => {
-         return n === driver
+//traditional syntax for functions:
+// function findMatching(drivers, name) {
+//      }
+//Arrow function syntax. This is the same as above: 
+findMatching = (drivers, name) => {
+     return drivers.filter(n => {
+         return n.toLowerCase() === name.toLowerCase() 
       }
     )
 }
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter( possibleMatch =>
+        possibleMatch.toLowerCase().indexOf(string.toLowerCase()) === 0
+        )
+    }
+        // Arrow function syntax: 
+        // fuzzyMatch = (drivers, string) => {
+        //     return drivers.filter(possibleMatch => {
+        //         return possibleMatch.toLowerCase().indexOf(string.toLowerCase()) === 0
+        //         }
+        //     )
+        // }
+
+    
+        function matchName(drivers, name) {
+            return drivers.filter( record => record.name === name)
+          }
